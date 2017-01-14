2b1d1adf5d230b0f2c7c3c8b4752877f8154bf9e
Danilo Mezgec <danilo@itech.media>
2017-01-14 20:00:21 +0000

    nodemon added

--- test/test.js
+++ test/test.js
@@ -1,4 +1,14 @@
+/**
+ * Given a list of urls
+ * When a certain time elapsed from the previous test run
+ * Run a new test over all the urls
+ */
+
 var assert = require('assert');
+describe('WPT tests', function () {
+    describe('single test')
+});
+
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
