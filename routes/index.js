3da7d28f34b74377877732802075dd195697ff5c
Danilo Mezgec <danilo@itech.media>
2017-01-14 17:50:17 +0000

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-    res.sendfile('views/index.jade');
+  res.render('index', { title: 'Express' });
+    // res.sendfile('views/index.jade');
 });
 
 router.get('/users', function(req, res, next) {
